5bae9834f5eb6ed591fde9fc142449c17eb837f3
Amit Batra <amitbatra3101@gmail.com>
2021-09-05 17:45:22 +0530

    Add login Page

---
+++ js/login.js
@@ -0,0 +1,10 @@
+var registered = false;
+
+if(registered){
+    document.querySelector(".info-content h2").innerHTML = "Login to your Account";
+    document.querySelector("#firstname").remove();
+    document.querySelector("#lastname").remove();
+    document.querySelector("#confirm_pass").remove();
+    document.querySelector("button h3").innerText = "Login";
+
+}
